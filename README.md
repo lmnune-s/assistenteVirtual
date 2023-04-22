a25ee895768420f000fe73cfc2676f874649e7ac
lmnune-s <larisam0563@gmail.com>
2023-04-22 11:49:20 -0300

    criacao projeto assistente virtual

---
+++ README.md
@@ -0,0 +1,41 @@
+# Projeto Inteligência Artificial - ADS Noturno
+
+Projeto para compor a nota da disciplina de Inteligência Artificial do curso de Análise e Desenvolvimento de Sistema da Fatec de Mogi Mirim.
+
+O projeto consiste em uma assistente virtual construída em Python, com o intuíto de ajudar em funções básicas como falar o dia e hora atual, e também a abrir alguns aplicativos.
+
+## Equipe
+* Ariadne Tagliaferro
+* José J. A. Oliveira Júnior
+* Larissa Martinez Nunes
+* Mateus Cardoso de Oliveira
+
+## Features:
+* **Horário Atual:** "Que horas são?"
+* **Data atual:** "Que dia é hoje?"
+* **Pesquisa no Google:** "Pesquisar inteligência artificial no Google."
+* **Pesquisa no Youtube:** "Pesquisar MPB no Youtube."
+* **Cotação de dólar, euro ou bitcoin:** "Qual a cotação do dólar hoje?"
+* **Traduzir para inglês ou português:** "Traduzir para o inglês./Traduzir para o português."
+* **Abrir programas em sua máquina:** "Abrir spotify."
+* **Desligar computador em um minuto ou cinco minutos:** "Desligar computador em um minuto."
+* **Cancelar o desligamento:** "Cancelar desligamento."
+* **Ouvir música no youtube:** "Ouvir música."
+
+## Tecnologias utilizadas:
+
+* [Python](https://www.python.org/): linguagem de programação
+* [Speech Recognition](https://pypi.org/project/SpeechRecognition/): reconhecimento de voz
+* [gTTS](https://pypi.org/project/gTTS/): sintetização de voz
+* [Playsound](https://pypi.org/project/playsound/): executador de áudio
+* [Translate](https://pypi.org/project/translate/)
+* Outras: os, sys, webbrowser, urllib.request, json, datetime, requests
+
+## Como executar:
+
+Para executar o projeto siga os seguintes passos:
+
+1. Clone o projeto a partir do repositório;
+2. Abra o projeto em uma IDE de sua preferência, seria interessante utilizar o PyCharm para maior facilidade;
+3. Instale as dependências do projeto;
+4. Rode o projeto através do comando 'python assistente.py' ou pelo botão 'run' do PyCharm.
---
+++ assistente.py
@@ -0,0 +1,170 @@
+import os
+import sys
+import webbrowser
+import pywhatkit
+
+import speech_recognition as sr
+import webbrowser as browser
+from gtts import gTTS
+from playsound import playsound
+from requests import get
+from translate import Translator
+from datetime import datetime
+from datetime import date
+import locale
+
+
+locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
+
+
+def cria_audio(audio, mensagem, lang='pt-br'):
+    tts = gTTS(mensagem, lang=lang)
+    tts.save(audio)
+    playsound(audio)
+    os.remove(audio)
+
+
+def monitora_audio():
+    recon = sr.Recognizer()
+    with sr.Microphone() as source:
+        while True:
+            print('Diga, o que você precisa?')
+            audio = recon.listen(source)
+            try:
+                mensagem = recon.recognize_google(audio, language='pt-br')
+                mensagem = mensagem.lower()
+                print('Você disse: ', mensagem)
+                executa_comandos(mensagem)
+                break
+            except sr.UnknownValueError:
+                pass
+            except sr.RequestError:
+                pass
+        return mensagem
+
+
+def cotacao(moeda):
+    requisicao = get(f'https://economia.awesomeapi.com.br/all/{moeda}-BRL')
+    cotacao = requisicao.json()
+    nome = cotacao[moeda]['name']
+    data = cotacao[moeda]['create_date']
+    valor = cotacao[moeda]['bid']
+    cria_audio("cotacao.mp3", f"Cotação do {nome} em {data} é {valor}")
+
+
+def tradutor(traducao):
+    if traducao == 'inglês':
+        traduz = Translator(from_lang="pt-br", to_lang='english')
+        cria_audio("traducao.mp3", "O que você gostaria de traduzir para o inglês?")
+        mensagem = monitora_audio()
+        traduzido = traduz.translate(mensagem)
+        cria_audio("traducao.mp3", f"A tradução de {mensagem} é")
+        cria_audio("traducao_eng.mp3", traduzido, lang='en')
+    elif traducao == 'português':
+        traduz = Translator(from_lang="english", to_lang='pt-br')
+        cria_audio("traducao.mp3", "O que você gostaria de traduzir para o português?")
+        mensagem = monitora_audio()
+        traduzido = traduz.translate(mensagem)
+        cria_audio("traducao.mp3", f"A tradução de")
+        cria_audio("traducao_eng.mp3", mensagem, lang='en')
+        cria_audio('traducao_port.mp3', f"é {traduzido}")
+
+
+def play_music(music_name):
+    url = "https://www.youtube.com/results?search_query=" + music_name
+    webbrowser.open(url)
+    pywhatkit.playonyt(music_name)
+
+
+def ask_music():
+    cria_audio("musica.mp3", "O que você gostaria de ouvir?")
+    mensagem = monitora_audio()
+    play_music(mensagem)
+
+def executa_comandos(mensagem):
+    # fechar assistente
+    if 'fechar assistente' in mensagem:
+        sys.exit()
+
+    # hora atual
+    elif 'horas' in mensagem:
+        hora = datetime.now().strftime('%H:%M')
+        frase = f"Agora são {hora}"
+        cria_audio('horas.mp3', frase)
+
+    # dta atual
+    elif 'dia' in mensagem:
+        data = date.today().strftime('%d de %B de %Y')
+        frase = f"Hoje é dia {data}"
+        cria_audio('data.mp3', frase)
+
+    # desligar o computador
+    elif 'desligar computador' in mensagem and 'em um minuto' in mensagem:
+        os.system("shutdown -s -t 60")
+    elif 'desligar computador' in mensagem and 'em cinco minutos' in mensagem:
+        os.system("shutdown -s -t 300")
+    elif 'cancelar desligamento' in mensagem:
+        os.system("shutdown -a")
+
+    # pesquisa no google
+    elif 'pesquisar' in mensagem and 'no google' in mensagem:
+        mensagem = mensagem.replace('pesquisar', '')
+        mensagem = mensagem.replace('no google', '')
+        browser.open(f'https://google.com/search?q={mensagem}')
+
+    # pesquisa no youtube
+    elif 'pesquisar' in mensagem and 'no youtube' in mensagem:
+        mensagem = mensagem.replace('pesquisar', '')
+        mensagem = mensagem.replace('no youtube', '')
+        browser.open(f'https://youtube.com/results?search_query={mensagem}')
+
+    # spotify
+    # elif 'ouvir' in mensagem and 'música' in mensagem:
+    #     browser.open('https://open.spotify.com/artist/6XyY86QOPPrYVGvF9ch6wz')
+
+    # ouvir musica no youtube
+    elif 'ouvir' in mensagem and 'música' in mensagem:
+        ask_music()
+
+
+    # cotação de moedas
+    elif 'dólar' in mensagem:
+        cotacao('USD')
+    elif 'euro' in mensagem:
+        cotacao('EUR')
+    elif 'bitcoin' in mensagem:
+        cotacao('BTC')
+
+
+    # abrir programas do computador
+    elif 'abrir' in mensagem and 'google chrome' in mensagem:
+        os.startfile("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")
+    elif 'abrir' in mensagem and 'spotify' in mensagem:
+        os.startfile("C:\\Users\\lmnun\\AppData\\Local\\Microsoft\\WindowsApps\\spotify.exe")
+    elif 'abrir' in mensagem and 'discord' in mensagem:
+        os.startfile("C:\\Users\\lmnun\\AppData\\Local\\Discord\\app-1.0.9012\\discord.exe")
+
+    # abrir programas windows
+    elif 'abrir' in mensagem and 'bloco de notas' in mensagem:
+        os.system("notepad.exe")
+    elif 'abrir' in mensagem and 'ferramenta de desenho' in mensagem:
+        os.system("mspaint.exe")
+    elif 'abrir' in mensagem and 'ferramenta de slides' in mensagem:
+        os.system("powerpnt.exe")
+    elif 'abrir' in mensagem and 'ferramenta de texto' in mensagem:
+        os.system("winword.exe")
+
+    # tradutor
+    elif 'traduzir' in mensagem and 'inglês' in mensagem:
+        tradutor('inglês')
+    elif 'traduzir' in mensagem and 'português' in mensagem:
+        tradutor('português')
+
+
+def main():
+    cria_audio("ola.mp3", "Olá")
+    while True:
+        monitora_audio()
+
+
+main()
